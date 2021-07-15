6e2ec20f9a071a79555a7c37c8549bc1839fd922
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-07-15 07:44:16 +0200

    add text-to-speech синтезатор add SpeechRecognizer - преобразование голоса в текст

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 
 import android.util.Log
 import android.view.Menu
@@ -26,6 +29,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.lang.StringBuilder
+import java.util.*
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "MainActivity"
@@ -41,12 +46,20 @@ class MainActivity : AppCompatActivity() {
 
     val pods = mutableListOf<HashMap<String, String>>()
 
+    //Google Voice
+    lateinit var textToSpeech: TextToSpeech
+
+    var isTtsReady: Boolean = false
+
+    val VOICE_RECOGNITION_REQUEST_CODE: Int = 777
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
         initWolframEngine()
+        initTts()
 
 
     }
@@ -80,10 +93,24 @@ class MainActivity : AppCompatActivity() {
 
         podsList.adapter = podsAdapter
 
+        //Google Voice
+        podsList.setOnItemClickListener { parent, view, position, id ->
+            if (isTtsReady) {
+                val title = pods[position]["Title"]
+                val content = pods[position]["Content"]
+                textToSpeech.speak(content, TextToSpeech.QUEUE_FLUSH, null, title)
+            }
+        }
+
         val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
 
         voiceInputButton.setOnClickListener {
-            Log.d(TAG, "FAB")
+            pods.clear()
+            podsAdapter.notifyDataSetChanged()
+            if (isTtsReady){
+                textToSpeech.stop()
+            }
+            showVoiceInputDialog()
         }
 
         progressBar = findViewById(R.id.progress_bar)
@@ -99,7 +126,9 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_stop -> {
-                Log.d(TAG, "action_stop")
+                if (isTtsReady) {
+                    textToSpeech.stop()
+                }
                 return true
             }
             R.id.action_clear -> {
@@ -179,5 +208,45 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    //Google Voice
+    fun initTts() {
+        textToSpeech = TextToSpeech(this) { code ->
+            if (code != TextToSpeech.SUCCESS) {
+                Log.e(TAG, "TTS error code: $code")
+                showSnackbar(getString(R.string.error_tts_is_not_ready))
+            } else {
+                isTtsReady = true
+            }
+
+        }
+        textToSpeech.language = Locale.US
+    }
+
+    //голосовой ввод
+    fun showVoiceInputDialog() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_PROMPT, getString((R.string.request_hint)))
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US)
+        }
+        runCatching {
+            startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE)
+        }.onFailure { t ->
+            showSnackbar(t.message ?: getString(R.string.error_voice_recognition_unavailable))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if ( requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK)
+            data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0)?.let { question ->
+                requestInput.setText(question)
+                askWolfram(question)
+            }
     }
 }
